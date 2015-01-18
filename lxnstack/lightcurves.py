Fixes error propagation in absolute magnitude computation

lxnstack/lightcurves.py
@@ -20,10 +20,11 @@
 # - http://www.britastro.org/vss/ccd_photometry.htm
 # - http://www.physics.csbsju.edu/370/photometry/manuals/OU.edu_CCD_photometry_wrccd06.pdf
 
-import numpy as np
-import plotting
 import utils
-
+import plotting
+import numpy as np
+import scipy as sp
+import scipy.stats
 
 class LightCurvePlot(plotting.Plot):
 
@@ -62,20 +63,65 @@ def getInstMagnitudeADU(star, ndimg=None):
             if (p <= or2) and (p > mr2):
                 bkg_adu.append(ndimg[sty+y, stx+x])
 
-    val_adu = np.array(val_adu)
-    bkg_adu = np.array(bkg_adu)
+    # This is basically a counting experiment and hence the
+    # Poisson probability distribution can be applied.
+    # In such situation, if for each photodiode (pixel) k we
+    # measure a number of photons A_k, the best value for its
+    # uncertainty is DA_k = sqrt(A_k)
 
+    val_adu = np.array(val_adu)
     total_star_pixels = len(val_adu)
 
-    total_val_adu = val_adu.sum(0)  # total ADU value for the star
-    mean_bkg_adu = bkg_adu.mean(0)  # average for the background
+    # NOTE(1):
+    #     we use sigmaclip here to remove evetual cosmic rays
+    #     or hot-pixels present in the sky background
+    bkg_adu = sp.stats.sigmaclip(np.array(bkg_adu),4,4)[0]
 
-    total_val_adu_delta = val_adu.shape[0]  # error value for the star
-    mean_bkg_adu_sigma = bkg_adu.std(0)  # average for the background
+    # val_adu_sigma = np.sqrt(val_adu)
+    # bkg_adu_sigma = np.sqrt(bkg_adu)
+    # Computing these values is only a waste of resources, see NOTE(2)
+
+
+    # These are the total counts of ADUs (directly poportional
+    # to the number of photons hitting the photodiode, unless
+    # we are near the saturation)
+    total_val_adu = val_adu.sum(0)
+    mean_bkg_adu = bkg_adu.mean(0)
+
+    # NOTE(2): 
+    #     Applying the error propagation, the error for total_val_adu
+    #     should be:
+    #
+    #         total_val_adu_sigma = np.sqrt((val_adu_sigma**2).sum(0))
+    #
+    #     however, a simple calculation leads to the following value:
+    #
+    #         total_val_adu_sigma = np.sqrt((val_adu_sigma**2).sum(0)) =
+    #         = np.sqrt((np.sqrt(val_adu)**2).sum(0)) =
+    #         = np.sqrt((val_adu).sum(0)) =
+    #         = np.sqrt(total_val_adu))
+    #
+    #     as espected for the Poisson probability distribution.
+    #     A similar calculation ca be done for mean_bkg_adu_sigma:
+    #
+    #     if we define N = len(bkg_adu_sigma) = len(bkg_adu) then,
+    #     since mean_bkg_adu = bkg_adu.mean(0), we have
+    #
+    #         mean_bkg_adu_sigma = np.sqrt((bkg_adu_sigma**2).sum(0))/N =
+    #         = np.sqrt(bkg_adu.sum(0)) / N =
+    #         = np.sqrt(bkg_adu.sum(0) / N**2) =
+    #         = np.sqrt(bkg_adu.mean(0) / N) =
+    #         = np.sqrt(mean_bkg_adu / N) =
+    #         = np.sqrt(mean_bkg_adu / len(bkg_adu))
+
+    total_val_adu_sigma = np.sqrt(total_val_adu)
+    mean_bkg_adu_sigma = np.sqrt(mean_bkg_adu / len(bkg_adu))
 
     # best value for the star
     mean_adu = total_val_adu - mean_bkg_adu*total_star_pixels
-    mean_adu_delta = total_val_adu_delta + 3*mean_bkg_adu_sigma
+    delta_a = total_val_adu_sigma**2
+    delta_b = (total_star_pixels*mean_bkg_adu_sigma)**2
+    mean_adu_delta = np.sqrt(delta_a + delta_b)
 
     # this avoids negative or null value:
     if (mean_adu > 0).all():

lxnstack/main_app.py
@@ -5734,11 +5734,11 @@ class theApp(Qt.QObject):
 
                 # A simple error propagation (see below)
 
-                bb_rer=np.abs(bb.getYError()/(bb.getYData()*LOGE10))
-                vv_rer=np.abs(vv.getYError()/(vv.getYData()*LOGE10))
+                bb_rer=(bb.getYError()/(bb.getYData()*LOGE10))**2
+                vv_rer=(vv.getYError()/(vv.getYData()*LOGE10))**2
 
-                star_bv_error = 2.5 * (bb_rer+vv_rer)                    
-                star_bv_error = np.array(star_bv_error)
+                star_bv_error2 = 6.25 * (bb_rer+vv_rer)        
+                star_bv_error2 = np.array(star_bv_error2)
             
             abs_mag_data = ([], [], [], [])
             for rf_name in  ref_stars:
@@ -5758,12 +5758,13 @@ class theApp(Qt.QObject):
                     
                     # A simple error propagation (see below)
 
-                    rbb_rer = np.abs(rbb.getYError()/(rbb.getYData()*LOGE10))
-                    rvv_rer = np.abs(rvv.getYError()/(rvv.getYData()*LOGE10))
-                    ref_bv_error = 2.5 * (rbb_rer+rvv_rer)
+                    rbb_rer = (rbb.getYError()/(rbb.getYData()*LOGE10))**2
+                    rvv_rer = (rvv.getYError()/(rvv.getYData()*LOGE10))**2
+                    ref_bv_error2 = 6.25 * (rbb_rer+rvv_rer)
                     
                     color_dif = instr_clr_corr * (star_bv_index-ref_bv_index)
-                    color_err = instr_clr_corr * (star_bv_error+ref_bv_error)
+                    color_err = instr_clr_corr
+                    color_err *= np.sqrt(star_bv_error2 + ref_bv_error2)
                     
                     # We shall use the total flux of the star, but we only
                     # have measures of the flux (measured as ADU) in different
@@ -5797,7 +5798,7 @@ class theApp(Qt.QObject):
                     #  
                     #  if V = f(A,B) then the error for V is
                     #
-                    #  DV = |DA*df(A,B)/dA| + |DB*df(A,B)/dB|
+                    #  DV = sqrt(|DA*df(A,B)/dA|**2 + |DB*df(A,B)/dB|**2)
                     #
                     #  for this reason the, since bol_mag is 
 
@@ -5809,13 +5810,13 @@ class theApp(Qt.QObject):
                     #
                     # the error on bol_mag is:
                     #
-                    #  bol_err = 2.5 * (|DA * df(A,B)/dA| + |DB * df(A,B)/dB|)
-                    #    = 2.5 * (|DA * 1/[A*ln(10)]| + |DB * -1/[B*ln(10)|)
-                    #    = 2.5 * (|DA/[A*ln(10)]| + |DB/[B*ln(10)]|)
+                    #  bol_err=2.5*sqrt(|DA*df(A,B)/dA|^2 + |DB*df(A,B)/dB|^2)
+                    #  = 2.5*sqrt(|DA*1/[A*ln(10)]|^2 + |DB * -1/[B*ln(10)|^2)
+                    #  = 2.5*sqrt(|DA/[A*ln(10)]|^2 + |DB/[B*ln(10)]|^2)
                     
-                    bol_err = np.abs(bol_str_yerr/(bol_str_ydat*LOGE10))
-                    bol_err += np.abs(bol_ref_yerr/(bol_ref_ydat*LOGE10))
-                    bol_err *= 2.5
+                    bol_erra = (bol_str_yerr/(bol_str_ydat*LOGE10))**2
+                    bol_errb = (bol_ref_yerr/(bol_ref_ydat*LOGE10))**2
+                    bol_err = 2.5 * np.sqrt(bol_erra + bol_errb)
 
                     # Transformation Equation
                     abs_mag = ref_mag - bol_mag - color_dif + airmas_corr
@@ -5841,9 +5842,9 @@ class theApp(Qt.QObject):
 
                     # A simple error propagation (see above)
 
-                    bol_err = np.abs(bol_str_yerr/(bol_str_ydat*LOGE10))
-                    bol_err += np.abs(bol_ref_yerr/(bol_ref_ydat*LOGE10))
-                    bol_err *= 2.5
+                    bol_erra = (bol_str_yerr/(bol_str_ydat*LOGE10))**2
+                    bol_errb = (bol_ref_yerr/(bol_ref_ydat*LOGE10))**2
+                    bol_err = 2.5 * np.sqrt(bol_erra + bol_errb)
                     
                     # NOTE: We have no color correction here because we have
                     #       only one spectral band